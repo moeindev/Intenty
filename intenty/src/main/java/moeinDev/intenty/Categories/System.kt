d79f4aad2f79c53607e5ad11f877fe186be3d3a7
moein <Moeinabdi77@gmail.com>
2018-11-26 18:46:07 +0330

    Added alarm intent

--- intenty/src/main/java/moeinDev/intenty/Categories/System.kt
+++ intenty/src/main/java/moeinDev/intenty/Categories/System.kt
@@ -3,6 +3,7 @@ package moeinDev.intenty.Categories
 import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.provider.AlarmClock
 import android.support.annotation.RequiresPermission
 
 
@@ -21,8 +22,15 @@ class System(val context: Context) {
     </intent-filter>
      */
     @RequiresPermission(Manifest.permission.SET_ALARM)
-    fun setAlarm(){
-
+    fun setAlarm(message: String,hour: Int,minutes: Int){
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_HOUR, hour)
+            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+        }
+        if (intent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(intent)
+        }
     }
 
     @RequiresPermission(Manifest.permission.SET_ALARM)
@@ -34,4 +42,5 @@ class System(val context: Context) {
     /*
     Set Alarm intent.
      */
+
 }
